ee459a110971d54c11fc54cc7c56122c88e56f8b
arnavtiet <arnavch0909@gmail.com>
2024-01-21 19:30:48 +0530

    21 jan

---
+++ src/NAV BAR/NAV.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import './NAV.css'
+import { FaCalculator } from "react-icons/fa6";
+function NAV() {
+  return (
+    <div className='navigation'>
+        
+          
+        <h1>THE CALCULATOR</h1>
+        
+         
+      <div className='title-icon'>
+          <FaCalculator  />
+      </div>
+    </div>
+  )
+}
+
+export default NAV
---
+++ src/buttons/button.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+import React from "react"
+import './b.css'
+
+const Button = ({val}) => {
+  return (
+    <div>
+      <button>{val}</button>
+    </div>
+  )
+};
+
+export default Button;
